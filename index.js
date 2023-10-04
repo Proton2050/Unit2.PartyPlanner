683a8c36f396b8dde44ba796bcd2204da630bbae
Timothy Chang <123012347+Proton2050@users.noreply.github.com>
2023-10-04 12:05:29 -0700

    Complete

--- index.js
+++ index.js
@@ -57,17 +57,23 @@ function renderParties() {
 // Ask API to create a new party based on form data
 async function addParty(event) {
     event.preventDefault();
-
+    // Create a JS Date
+    const dateObj = new Date(addPartyForm.date.value);
+    
+    console.log(addPartyForm.date.value);
+    console.log(dateObj);
+    
     try {
         const response = await fetch(API_URL, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                name: name,
-                description: description,
-                date: date,
-                location: location,
-            }),
+                name: addPartyForm.name.value,
+                description: addPartyForm.description.value,
+                // make sure JS Date is in the right format
+                date: dateObj.toISOString(),
+                location: addPartyForm.location.value
+            })
         });
         addPartyForm.name.value = '';
         addPartyForm.description.value = '';
